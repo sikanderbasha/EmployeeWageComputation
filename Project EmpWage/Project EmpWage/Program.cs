ee9c118313086e87ccdf4d822679458b92508eb0
Sikander <sikku.rk405@gmail.com>
2022-01-27 21:50:31 +0530

    Added employee attendance program

---
+++ Project EmpWage/Project EmpWage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_EmpWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Employee Wage Program"); //main
+            WageComputation EmployeeWage = new WageComputation();
+            EmployeeWage.Attendance();
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Project EmpWage/Project EmpWage/WageComputation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_EmpWage
+{
+    class WageComputation
+    {
+        readonly int IS_PRESENT = 1;
+
+        public void Attendance()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_PRESENT)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+
+
+
+        }
+
+    }
+}
