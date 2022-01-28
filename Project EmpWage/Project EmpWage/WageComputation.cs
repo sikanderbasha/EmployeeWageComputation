2bcb307ba2c939de2cba8f72ae9986af865df375
Sikander <sikku.rk405@gmail.com>
2022-01-28 07:11:33 +0530

    Added switch case program

--- Project EmpWage/Project EmpWage/WageComputation.cs
+++ Project EmpWage/Project EmpWage/WageComputation.cs
@@ -6,29 +6,33 @@ using System.Threading.Tasks;
 
 namespace Project_EmpWage
 {
-    class WageComputation
+    public class WageComputation
     {
-        readonly int Wage_Per_Hours = 20;
-        readonly int Emp_Part_Time = 1;
-        int empHrs;
-        int EmployeeWage;
+        const int Emp_Full_Time = 1;
+        const int Emp_Part_Time = 2;
+        public int Wage_Per_Hours = 20;
+        public int empHrs = 0;
+
         public void Attendance()
         {
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == Emp_Part_Time)
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
             {
-                Console.WriteLine("Employee is Present");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
-            }
-            EmployeeWage = empHrs * Wage_Per_Hours;
-            Console.WriteLine("Employee Full Wage is : " + EmployeeWage);
+                case Emp_Full_Time:
+                    empHrs = 8;
+                    break;
 
+                case Emp_Part_Time:
+                    empHrs = 4;
+                    break;
+
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            int EmployeeWage = empHrs * Wage_Per_Hours;
+            Console.WriteLine(EmployeeWage);
         }
     }
 
