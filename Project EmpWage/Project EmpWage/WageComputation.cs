6221c7051d01ea9a3b7486256e807ca563e3a827
Sikander <sikku.rk405@gmail.com>
2022-01-28 07:34:34 +0530

    Added total working hours program

--- Project EmpWage/Project EmpWage/WageComputation.cs
+++ Project EmpWage/Project EmpWage/WageComputation.cs
@@ -12,14 +12,16 @@ namespace Project_EmpWage
         const int Emp_Part_Time = 2;
         public int Wage_Per_Hours = 20;
         public int empHrs = 0;
-        public int Working_Days = 20;
-        public int Salary = 0;
-
+        public int Working_Days_Per_Month = 20;
+        public int Salary_Month = 100;
+        public int Working_Hours = 100;
+        public int Working_Days = 0;
 
         public void Attendance()
         {
-            for (int day = 1; day <= Working_Days; day++)
+            while (empHrs < Working_Hours && Working_Days < Working_Days_Per_Month)
             {
+                Working_Days++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -36,13 +38,12 @@ namespace Project_EmpWage
                         empHrs = 0;
                         break;
                 }
-               
-           
-        int EmployeeWage = empHrs * Wage_Per_Hours;
-        Salary += EmployeeWage;
-                Console.WriteLine(EmployeeWage);
+                Working_Hours += empHrs;
+
             }
-}
+            int EmpSalary = Working_Hours + Wage_Per_Hours;
+            Console.WriteLine(EmpSalary);
+        }
     }
 
 }
